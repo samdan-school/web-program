0e0b88d1e56b4eb1c661d202082c454eff945b35
Samdan D <pri.samdan@gmail.com>
2018-12-16 23:51:30 +0800

    almost

--- project/api/index.js
+++ project/api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const request = require('request');
+const fs = require('fs');
 
 let api = express.Router();
-
+// https://www.google.com/maps/@0,0,0a,90y,90t/data=!3m4!1e1!3m2!1sAF1QipMjhTEnSnmowCE2w7DUiJQSbxhxER2Y6-jfrJAR!2e10
 // Options
 const API_KEY = '?key=AIzaSyDzRNFAzXgtn-TlTjAwj9guLgHshytukpw';
-const ACCESS_TOKEN = 'ya29.Glt0BixGQ8apKc0MbGqW6n3DsCJUNkYMciXxy9LK07CLdyOXb7ffeq16w1SNA8MWyXVHeGDI0PAX3FMRCGfkJV2yIOeRp3Lco8Pb85tbkpIvgLceHqN_jgD-DJkf';
+const ACCESS_TOKEN = 'ya29.Glt0Bq10-skD4-Xo0DbdhScTx8xejdZdkWpSkPHy9cGSTAy5-sJPNz-Vrf7G4O9E5aG91Q1qsnzonekLJsqe97VWHfbCFidk7TwcShcRvnhSwik0TRiFb0HrDY2q';
 
 // *Get all photo
 const getPhotosURL = 'https://streetviewpublish.googleapis.com/v1/photos';
@@ -14,23 +15,13 @@ let getPhotosOption = {
     url: getPhotosURL + API_KEY,
 };
 
-let uploadAPhotoOptions = [
-    {
-        method: 'POST',
-        url: 'https://streetviewpublish.googleapis.com/v1/photo:startUpload' + API_KEY,
-        headers: {
-            'Content-Length': 0
-        }
-    }, {
-        method: 'POST',
-        url: 'https://streetviewpublish.googleapis.com/v1/photo:startUpload' + API_KEY,
-        headers: {
-            'Content-Length': 0
-        }
-    }, {
-
+let uploadAPhotoOptions = {
+    method: 'POST',
+    url: 'https://streetviewpublish.googleapis.com/v1/photo:startUpload' + API_KEY,
+    headers: {
+        'Content-Length': 0
     }
-];
+};
 
 // *Get a photo
 const getAPhoto = 'https://streetviewpublish.googleapis.com/v1/photo/'
@@ -56,30 +47,53 @@ api.get('/get-photos', (req, res) => {
 
 
 api.get('/upload-photo', (req, res) => {
-    request(uploadAPhotoOptions[0], (e, response, body) => {
+    request(uploadAPhotoOptions, (e, response, body) => {
         if (e) {
             res.send(e);
         }
         const uploadUrl = JSON.parse(body).uploadUrl;
         console.log(uploadUrl);
 
-        request.post({
+        fs.createReadStream('./images/room.jpg').pipe(request.post({
             url: uploadUrl,
             headers: {
-                'upload-file': 'https://photos.google.com/album/AF1QipMrpZHRcw9xQpk11IH2qOGXmRWFMMGoEzF-1MEk/photo/AF1QipN8MrutGS_pzBBI-VQDoa13YOqNq-1NBRITixIA'
             }
         }, (e, response, body) => {
             if (e) {
                 res.send(e);
             }
-            console.log(body);
-            res.send(response);
-        }).auth(null, null, true, ACCESS_TOKEN);
 
+            const updateInfo = {
+                method: 'POST',
+                url: 'https://streetviewpublish.googleapis.com/v1/photo' + API_KEY,
+                json: true,
+                body: {
+                    uploadReference: {
+                        uploadUrl: uploadUrl
+                    },
+                    pose: {
+                        heading: 0,
+                        latLngPair: {
+                            latitude: 47.891451,
+                            longitude: 106.896849
+                        }
+                    },
+                    captureTime: {
+                        seconds: 1544973844
+                    },
+
+                },
+            };
+            request(updateInfo, (e, response, body) => {
+                if (e) {
+                    res.send(e);
+                }
+
+                console.log(body);
+                res.send(response);
+            }).auth(null, null, true, ACCESS_TOKEN);
+        }).auth(null, null, true, ACCESS_TOKEN));
     }).auth(null, null, true, ACCESS_TOKEN);
 });
 
 module.exports = api;
-// curl --request GET \
-//     --url 'https://streetviewpublish.googleapis.com/v1/photos?key=AIzaSyDzRNFAzXgtn-TlTjAwj9guLgHshytukpw' \
-//     --header 'authorization: Bearer ya29.GltyBn_C4zHtTb1jYFTl0ibPJLDi6dR2cI10Yl2sF1nHM93NDLsGWiC7kLFD6PpNGC0s0_zDqb6LAhasglA81AMQm07eBUejK3kEwkgjcTDfiH3MbKTIayBdbzxR'
