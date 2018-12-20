ccc8db8a9f79aa65b00298147afb755b35fac9cf
Samdan D <pri.samdan@gmail.com>
2018-12-21 01:21:33 +0800

    crud finished

--- project/api/function.js
+++ project/api/function.js
@@ -8,54 +8,114 @@ const API_KEY = process.env.API_KEY;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 let svpa = {
+    /*
+    getAllPhotos - Энэ функц нь хэрэглэгчийн оруулсан зураагуудын мэдээлэлийг харна.
+
+    Params:
+        callback function - энэ нь 3 аргументтэй
+            1. Алдаа                - error
+            2. Хариултийн объект    - request object 
+            3. Хариултийн эх бие    - response body
+     */
     getAllPhotos: function (callback) {
-        let photosInfo =  request({
+        let photosInfo = request({
             method: 'GET',
             url: 'https://streetviewpublish.googleapis.com/v1/photos' + '?key=' + API_KEY,
-        }, (e, response, body) => {
-            if (e) {
-                callback(e);
+            'auth': {
+                'bearer': ACCESS_TOKEN
             }
-            callback(body);
-        }).auth(null, null, true, ACCESS_TOKEN);
+        }, (error, response, body) => {
+            callback(error, response, body);
+        });
     },
-    getAPhotos: function (photoId) {
+    /*
+    getAPhotos - Энэ функц нь хэрэглэгчийн оруулсан photoId-тай зурагийн мэдээлэлийг авна.
+
+
+    Params:
+        photoId :   string   - Зурагийн дугаар
+        callback:   function - энэ нь 3 аргументтэй
+            1. Алдаа                - error
+            2. Хариултийн объект    - request object 
+            3. Хариултийн эх бие    - response body
+     */
+    getAPhoto: function (photoId, callback) {
         request({
             method: 'GET',
-            url: 'https://streetviewpublish.googleapis.com/v1/photo/' + photoId + '?key=' + API_KEY
-        }, (e, response, body) => {
-            if (e) {
-                return e;
+            url: 'https://streetviewpublish.googleapis.com/v1/photo/' + photoId + '?key=' + API_KEY,
+            'auth': {
+                'bearer': ACCESS_TOKEN
             }
-            return body;
-        }).auth(null, null, true, ACCESS_TOKEN);
+        }, (error, response, body) => {
+            callback(error, response, body);
+        });
     },
-    uploadAPhoto: function (photoInfo) {
+    /*
+    uploadAPhoto - Энэ функц нь хэрэглэгчийн оруулсан зурагыг Street View руу оруулна.
+
+
+    Params:
+        photoInfo   :   Object      - Зурагий талаарах мэдээлэл - үүнийг (https://developers.google.com/streetview/publish/reference/rest/v1/photo) дэлэгрэгүй харна уу.
+        callback    :   function    - энэ нь 3 аргументтэй
+            1. Алдаа                - error
+            2. Хариултийн объект    - request object 
+            3. Хариултийн эх бие    - response body
+     */
+    uploadAPhoto: function (photoInfo = {}, callback) {
+        // Zaaval baih yctoa talbaruud
+        const mustIncludePhotoInfo = ['photoPath', 'heading', 'latitude', 'longitude', 'captureTime'];
+        // deereh talbar baigaa hesgiig shalgaj baina
+        const missingInfo = mustIncludePhotoInfo.filter((info) => {
+            return !(info in photoInfo);
+        });
+
+        // herev dutuu talbar n 0 ees ih ued aldaa butsaana
+        if (missingInfo.length > 0) {
+            const error = {
+                'info': 'missing photo information!',
+                'missing': missingInfo
+            }
+            callback(error, null, null);
+        }
+        // Zuragiin zamiin shalgaj baina
+        if (!fs.existsSync(photoInfo.photoPath)) {
+            callback('Photo path is wrong!', null, null);
+        }
+
         request({
             method: 'POST',
-            url: 'https://streetviewpublish.googleapis.com/v1/photo:startUpload' + API_KEY,
+            url: 'https://streetviewpublish.googleapis.com/v1/photo:startUpload?key=' + API_KEY,
+            'auth': {
+                'bearer': ACCESS_TOKEN
+            },
             headers: {
                 'Content-Length': 0
-            }
-        }, (e, response, body) => {
-            if (e) {
-                return e;
+            },
+        }, (error, response, body) => {
+            if (error) {
+                callback(error, response, body);
             }
             // Zuragaa upload hii url irne
             const uploadUrl = JSON.parse(body).uploadUrl;
 
             // upload hiih url ruu zuragaa hiij ugnu
-            fs.createReadStream('./images/room.jpg').pipe(request.post({
-                url: uploadUrl
-            }, (e, response, body) => {
-                if (e) {
-                    return e;
+            fs.createReadStream(photoInfo.photoPath).pipe(request.post({
+                url: uploadUrl,
+                'auth': {
+                    'bearer': ACCESS_TOKEN
+                }
+            }, (error, response, body) => {
+                if (error) {
+                    callback(error, response, body);
                 }
 
                 const updateInfo = {
                     method: 'POST',
                     url: 'https://streetviewpublish.googleapis.com/v1/photo' + '?key=' + API_KEY,
                     json: true,
+                    'auth': {
+                        'bearer': ACCESS_TOKEN
+                    },
                     body: {
                         uploadReference: {
                             uploadUrl: uploadUrl
@@ -68,19 +128,63 @@ let svpa = {
                             }
                         },
                         captureTime: {
-                            seconds: photoInfo.captureSeconds
+                            seconds: photoInfo.captureTime
                         },
                     },
                 };
-                request(updateInfo, (e, response, body) => {
-                    if (e) {
-                        return e;
-                    }
-                    return body;
-                }).auth(null, null, true, ACCESS_TOKEN);
-            }).auth(null, null, true, ACCESS_TOKEN));
-        }).auth(null, null, true, ACCESS_TOKEN);
+                // amjilttai ued zuragiin id butsaana 
+                request(updateInfo, (error, response, body) => {
+                    callback(error, response, body);
+                });
+            }));
+        });
     },
+    /*
+    updatePhotos - Энэ функц нь Street View-д байгаа зурагуудын мэдээлэлийг өөрчилнө.
+
+    Params:
+        photosUpdateInfo    :   Объектэн хүснэгт- Шинэчлэх зурагуудын мэдээлэл
+        callback            :   function        - энэ нь 3 аргументтэй
+            1. Алдаа                - error
+            2. Хариултийн объект    - request object 
+            3. Хариултийн эх бие    - response body
+     */
+    updatePhotos: function (photosUpdateInfo = [], callback) {
+        request({
+            method: 'POST',
+            url: 'https://streetviewpublish.googleapis.com/v1/photos:batchUpdate' + '?key=' + API_KEY,
+            'auth': {
+                'bearer': ACCESS_TOKEN
+            },
+            json: true,
+            body: {
+                'updatePhotoRequests': photosUpdateInfo
+            }
+        }, (error, response, body) => {
+            callback(error, response, body);
+        });
+    },
+    /*
+    deleteAPhoto - Энэ функц нь Street View-д байгаа зурагийг устгна.
+
+    Params:
+        photoId     :   string      - Зурагийн дугаар
+        callback    :   function    - энэ нь 3 аргументтэй
+            1. Алдаа                - error
+            2. Хариултийн объект    - request object 
+            3. Хариултийн эх бие    - response body
+     */
+    deleteAPhoto: function (photoId, callback) {
+        request({
+            method: 'DELETE',
+            url: 'https://streetviewpublish.googleapis.com/v1/photo/' + photoId + '?key=' + API_KEY,
+            'auth': {
+                'bearer': ACCESS_TOKEN
+            }
+        }, (error, response, body) => {
+            callback(error, response, body);
+        });
+    }
 }
 
 module.exports = svpa;
--- project/api/index.js
+++ project/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const fs = require('fs');
+const path = require('path');
 
 const svpa = require('./function');
 
@@ -13,14 +14,99 @@ const getAPhoto = 'https://streetviewpublish.googleapis.com/v1/photo/'
 // 1. Request an Upload URL
 
 api.get('/get-photos', (req, res) => {
-    svpa.getAllPhotos((api_response) => {
+    svpa.getAllPhotos((error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(JSON.parse(api_response), null, 3));
+        res.send(JSON.parse(body), null, 3);
     });
 });
 
+api.get('/get-photo', (req, res) => {
+    const photoId = 'CAoSLEFGMVFpcE1WeXdPRFJpcjV3MXZtWVp0a1BrX3U5NFhfUm5uX1pVLXZDY2RH';
+    svpa.getAPhoto(photoId, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.parse(body), null, 3);
+    });
+});
 
 api.get('/upload-photo', (req, res) => {
+    const photoInfos = [{
+        photoPath: path.join(__dirname, '../' + 'images/', 'school_1.jpg'),
+        heading: 0,
+        latitude: 47.922778,
+        longitude: 106.921056,
+        captureTime: 1545325560
+    }, {
+        photoPath: path.join(__dirname, '../' + 'images/', 'school_2.jpg'),
+        heading: 0,
+        latitude: 47.922784,
+        longitude: 106.920971,
+        captureTime: 1545325560
+    }];
+
+    svpa.uploadAPhoto(photoInfos[1], (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+
+        res.send(body);
+    });
+});
+
+api.get('/update-photo', (req, res) => {
+    const photosUpdateInfo = [{
+        'photo': {
+            'photoId': {
+                'id': 'CAoSLEFGMVFpcE1WeXdPRFJpcjV3MXZtWVp0a1BrX3U5NFhfUm5uX1pVLXZDY2RH'
+            },
+            'connections': [
+                {
+                    'target': {
+                        'id': 'CAoSLEFGMVFpcE81TXpzT210cGttblZFVW5GS1VkcFBtZDZuUk5RaWluX0lIRUhF'
+                    }
+                }
+            ]
+        },
+        'updateMask': 'connections'
+    }, {
+        'photo': {
+            'photoId': {
+                'id': 'CAoSLEFGMVFpcE81TXpzT210cGttblZFVW5GS1VkcFBtZDZuUk5RaWluX0lIRUhF'
+            },
+            'connections': [
+                {
+                    'target': {
+                        'id': 'CAoSLEFGMVFpcE1WeXdPRFJpcjV3MXZtWVp0a1BrX3U5NFhfUm5uX1pVLXZDY2RH'
+                    }
+                }
+            ]
+        },
+        'updateMask': 'connections'
+    }];
+
+    svpa.updatePhotos(photosUpdateInfo, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+
+        res.send(body);
+    });
+});
+
+api.get('/delete-photo', (req, res) => {
+    const photoId = 'CAoSLEFGMVFpcFAxcUJlZUJ5aDR5T1U4ajJxVnBUQkhrVVRHeHV1NjJPNXhGT2du';
+    svpa.deleteAPhoto(photoId, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.parse(body), null, 3);
+    });
 });
 
 module.exports = api;
